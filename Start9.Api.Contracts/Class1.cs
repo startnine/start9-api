f8088ebf1b4033b95094d937d3a92acb5f23bb75
John T <reflectronic@gmail.com>
2018-02-22 20:43:38 -0500

    added (temporary) contracts projects for add-in pipeline

---
+++ Start9.Api.Contracts/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System.AddIn.Contract;
+using System.AddIn.Pipeline;
+
+namespace Start9.Api.Contracts
+{
+	[AddInContract]
+	public interface ICalculatorContract : IContract
+	{
+		double Add(double a, double b);
+		double Subtract(double a, double b);
+		double Multiply(double a, double b);
+		double Divide(double a, double b);
+	}
+}
