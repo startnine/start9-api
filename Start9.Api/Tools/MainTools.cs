2cfa8b37889faad8972795545e9c02691a85dda2
Rob55Rod AKA Splitwirez <Splitwirez@gmail.com>
2018-02-25 08:37:22 -0500

    Added ShortcutTools

--- Start9.Api/Tools/MainTools.cs
+++ Start9.Api/Tools/MainTools.cs
@@ -10,6 +10,7 @@ using BitmapCacheOption = System.Windows.Media.Imaging.BitmapCacheOption;
 using System.IO;
 using System.Windows.Media.Imaging;
 using System.Windows.Interop;
+using System.Text;
 
 namespace Start9.Api.Tools
 {
@@ -55,6 +56,130 @@ namespace Start9.Api.Tools
 			Logpixelsy = 90
 		}
 	}
+
+    public static class ShortcutTools
+    {
+        public static string GetTargetPath(string filePath)
+        {
+            string targetPath = ResolveMsiShortcut(filePath);
+
+            if (targetPath == null)
+            {
+                targetPath = ResolveShortcut(filePath);
+            }
+
+            if (targetPath == null)
+            {
+                targetPath = GetInternetShortcut(filePath);
+            }
+
+            if (targetPath == null | targetPath == "" | targetPath.Replace(" ", "") == "")
+            {
+                return filePath;
+            }
+            else
+            {
+                return targetPath;
+            }
+        }
+
+        public static string GetInternetShortcut(string filePath)
+        {
+            try
+            {
+                string url = "";
+
+                using (TextReader reader = new StreamReader(filePath))
+                {
+                    string line = "";
+                    while ((line = reader.ReadLine()) != null)
+                    {
+                        if (line.StartsWith("URL="))
+                        {
+                            string[] splitLine = line.Split('=');
+                            if (splitLine.Length > 0)
+                            {
+                                url = splitLine[1];
+                                break;
+                            }
+                        }
+                    }
+                }
+                return url;
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+        static string ResolveShortcut(string filePath)
+        {
+            // IWshRuntimeLibrary is in the COM library "Windows Script Host Object Model"
+            IWshRuntimeLibrary.WshShell shell = new IWshRuntimeLibrary.WshShell();
+
+            try
+            {
+                IWshRuntimeLibrary.IWshShortcut shortcut = (IWshRuntimeLibrary.IWshShortcut)shell.CreateShortcut(filePath);
+                return shortcut.TargetPath;
+            }
+            catch
+            {
+                // A COMException is thrown if the file is not a valid shortcut (.lnk) file 
+                return null;
+            }
+        }
+
+        [DllImport("msi.dll", CharSet = CharSet.Auto)]
+        public static extern uint MsiGetShortcutTarget(string targetFile, StringBuilder productCode, StringBuilder featureID, StringBuilder componentCode);
+
+        [DllImport("msi.dll", CharSet = CharSet.Auto)]
+        public static extern InstallState MsiGetComponentPath(string productCode, string componentCode, StringBuilder componentPath, ref int componentPathBufferSize);
+
+        public const int MaxFeatureLength = 38;
+        public const int MaxGuidLength = 38;
+        public const int MaxPathLength = 1024;
+
+        public enum InstallState
+        {
+            NotUsed = -7,
+            BadConfig = -6,
+            Incomplete = -5,
+            SourceAbsent = -4,
+            MoreData = -3,
+            InvalidArg = -2,
+            Unknown = -1,
+            Broken = 0,
+            Advertised = 1,
+            Removed = 1,
+            Absent = 2,
+            Local = 3,
+            Source = 4,
+            Default = 5
+        }
+
+        static string ResolveMsiShortcut(string file)
+        {
+            StringBuilder product = new StringBuilder(MaxGuidLength + 1);
+            StringBuilder feature = new StringBuilder(MaxFeatureLength + 1);
+            StringBuilder component = new StringBuilder(MaxGuidLength + 1);
+
+            MsiGetShortcutTarget(file, product, feature, component);
+
+            int pathLength = MaxPathLength;
+            StringBuilder path = new StringBuilder(pathLength);
+
+            InstallState installState = MsiGetComponentPath(product.ToString(), component.ToString(), path, ref pathLength);
+            if (installState == InstallState.Local)
+            {
+                return path.ToString();
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
 
     public static class MiscTools
     {
