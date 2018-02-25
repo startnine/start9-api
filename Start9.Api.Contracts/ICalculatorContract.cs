23476ef728b62c2152db52fc68a70b923cdcb9ad
John T <reflectronic@gmail.com>
2018-02-25 15:07:50 -0500

    calculator -> books

--- Start9.Api.Contracts/ICalculatorContract.cs
+++ Start9.Api.Contracts/ICalculatorContract.cs
@@ -4,11 +4,33 @@ using System.AddIn.Pipeline;
 namespace Start9.Api.Contracts
 {
 	[AddInContract]
-	public interface ICalculatorContract : IContract
+	public interface ILibraryManagerContract : IContract
 	{
-		double Add(double a, double b);
-		double Subtract(double a, double b);
-		double Multiply(double a, double b);
-		double Divide(double a, double b);
+		// Pass a collection of books,
+		// of type IBookInfoContract
+		// to the add-in for processing.
+		void ProcessBooks(IListContract<IBookInfoContract> books);
+
+		// Get a IBookInfoContract object
+		// from the add-in of the
+		// the best selling book.
+		IBookInfoContract BestSeller { get; }
+
+		// This method has has arbitrary
+		// uses and shows how you can
+		// mix serializable and custom types.
+		string Data(string txt);
+	}
+
+	// Contains infomration about a book.
+	public interface IBookInfoContract : IContract
+	{
+		string Id { get; }
+		string Author { get; }
+		string Title { get; }
+		string Genre { get; }
+		string Price { get; }
+		string PublishDate { get; }
+		string Description { get; }
 	}
 }
