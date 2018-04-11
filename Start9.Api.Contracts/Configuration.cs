f5b2bc09200d96dc6a32f1169fe08e6637c34e86
John T <reflectronic@gmail.com>
2018-04-11 18:11:01 -0400

    updated the contract to be Start9 related

---
+++ Start9.Api.Contracts/Configuration.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace Start9.Api.Contracts
+{
+    [Serializable] 
+    public abstract class Configuration
+    {
+
+    }
+}
