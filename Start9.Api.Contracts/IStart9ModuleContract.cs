02057a9c04cd2d1cfcd0a75bd14efcd9e2c75f77
John T <reflectronic@gmail.com>
2018-04-22 09:30:57 -0400

    oops

--- Start9.Api.Contracts/IStart9ModuleContract.cs
+++ Start9.Api.Contracts/IStart9ModuleContract.cs
@@ -6,7 +6,7 @@ namespace Start9.Api.Contracts
     [AddInContract]
     public interface IModuleContract : IContract
     {
-        Message MessageRecieved<T>(Message<T> message);
+        Message MessageRecieved<T>(Message message);
         Configuration Configuration { get; }
         Skin Skin { get; set; }
     }
