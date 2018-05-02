8c14a8f2a63072e2699c041f655395898e51039b
Rob55Rod AKA Splitwirez <Splitwirez@gmail.com>
2018-05-02 12:01:53 -0400

    More reveal optimizations

--- Start9.Api/Controls/Reveal.cs
+++ Start9.Api/Controls/Reveal.cs
@@ -9,11 +9,118 @@ using System.Windows.Data;
 using System.Windows.Media;
 using Start9.Api.Tools;
 using Start9.Api;
+using System.Diagnostics;
 
 namespace Start9.Api.Controls
 {
     public class Reveal : Canvas
     {
+        //from https://stackoverflow.com/questions/1517743/in-wpf-how-can-i-determine-whether-a-control-is-visible-to-the-user#1517794
+        private bool isElementClickable<T>(UIElement container, UIElement element, out bool isPartiallyClickable)
+        {
+            isPartiallyClickable = false;
+            Rect pos = GetAbsolutePlacement((FrameworkElement)container, (FrameworkElement)element);
+            bool isTopLeftClickable = GetIsPointClickable<T>(container, element, new Point(pos.TopLeft.X + 1, pos.TopLeft.Y + 1));
+            bool isBottomLeftClickable = GetIsPointClickable<T>(container, element, new Point(pos.BottomLeft.X + 1, pos.BottomLeft.Y - 1));
+            bool isTopRightClickable = GetIsPointClickable<T>(container, element, new Point(pos.TopRight.X - 1, pos.TopRight.Y + 1));
+            bool isBottomRightClickable = GetIsPointClickable<T>(container, element, new Point(pos.BottomRight.X - 1, pos.BottomRight.Y - 1));
+
+            if (isTopLeftClickable || isBottomLeftClickable || isTopRightClickable || isBottomRightClickable)
+            {
+                isPartiallyClickable = true;
+            }
+
+            return isTopLeftClickable && isBottomLeftClickable && isTopRightClickable && isBottomRightClickable; // return if element is fully clickable
+        }
+
+        private bool GetIsPointClickable<T>(UIElement container, UIElement element, Point p)
+        {
+            DependencyObject hitTestResult = HitTest<T>(p, container);
+            if (null != hitTestResult)
+            {
+                return isElementChildOfElement(element, hitTestResult);
+            }
+            return false;
+        }
+
+        private DependencyObject HitTest<T>(Point p, UIElement container)
+        {
+            PointHitTestParameters parameter = new PointHitTestParameters(p);
+            DependencyObject hitTestResult = null;
+
+            HitTestResultCallback resultCallback = (result) =>
+            {
+                UIElement elemCandidateResult = result.VisualHit as UIElement;
+                // result can be collapsed! Even though documentation indicates otherwise
+                if (null != elemCandidateResult && elemCandidateResult.Visibility == Visibility.Visible)
+                {
+                    hitTestResult = result.VisualHit;
+                    return HitTestResultBehavior.Stop;
+                }
+
+                return HitTestResultBehavior.Continue;
+            };
+
+            HitTestFilterCallback filterCallBack = (potentialHitTestTarget) =>
+            {
+                if (potentialHitTestTarget is T)
+                {
+                    hitTestResult = potentialHitTestTarget;
+                    return HitTestFilterBehavior.Stop;
+                }
+
+                return HitTestFilterBehavior.Continue;
+            };
+
+            VisualTreeHelper.HitTest(container, filterCallBack, resultCallback, parameter);
+            return hitTestResult;
+        }
+
+        private bool isElementChildOfElement(DependencyObject child, DependencyObject parent)
+        {
+            if (child.GetHashCode() == parent.GetHashCode())
+                return true;
+            IEnumerable<DependencyObject> elemList = FindVisualChildren<DependencyObject>((DependencyObject)parent);
+            foreach (DependencyObject obj in elemList)
+            {
+                if (obj.GetHashCode() == child.GetHashCode())
+                    return true;
+            }
+            return false;
+        }
+
+        private IEnumerable<T> FindVisualChildren<T>(DependencyObject depObj) where T : DependencyObject
+        {
+            if (depObj != null)
+            {
+                for (int i = 0; i < VisualTreeHelper.GetChildrenCount(depObj); i++)
+                {
+                    DependencyObject child = VisualTreeHelper.GetChild(depObj, i);
+                    if (child != null && child is T)
+                    {
+                        yield return (T)child;
+                    }
+
+                    foreach (T childOfChild in FindVisualChildren<T>(child))
+                    {
+                        yield return childOfChild;
+                    }
+                }
+            }
+        }
+
+        private Rect GetAbsolutePlacement(FrameworkElement container, FrameworkElement element, bool relativeToScreen = false)
+        {
+            var absolutePos = element.PointToScreen(new System.Windows.Point(0, 0));
+            if (relativeToScreen)
+            {
+                return new Rect(absolutePos.X, absolutePos.Y, element.ActualWidth, element.ActualHeight);
+            }
+            var posMW = container.PointToScreen(new System.Windows.Point(0, 0));
+            absolutePos = new System.Windows.Point(absolutePos.X - posMW.X, absolutePos.Y - posMW.Y);
+            return new Rect(absolutePos.X, absolutePos.Y, element.ActualWidth, element.ActualHeight);
+        }
+
         Timer RevealGlowTimer = new Timer(1);
 
         public Brush Hover
@@ -135,9 +242,11 @@ namespace Start9.Api.Controls
             {
                 Dispatcher.Invoke(new Action(() =>
                 {
-                    if (IsVisible)
+                    bool isPartiallyClickable = true;
+                    bool isFullyClickable = isElementClickable<Button>(Window.GetWindow(this), this, out isPartiallyClickable);
+                    if (isPartiallyClickable)
                     {
-                        var c = SystemScaling.CursorPosition;
+                    var c = SystemScaling.CursorPosition;
                         var p = PointToScreen(new Point(0, 0));
                         var t = new Point((c.X - p.X) - (vis.Width / 2), (c.Y - p.Y) - (vis.Height / 2));
                         /*bg.Viewbox = new Rect(
@@ -150,6 +259,7 @@ namespace Start9.Api.Controls
                     }
                     else
                     {
+                        Debug.WriteLine("Reveal timer stopped");
                         RevealGlowTimer.Stop();
                     }
                 }));
