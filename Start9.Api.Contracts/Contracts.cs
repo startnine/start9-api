fc701f7fc8abf33cff1d13be98fa5054ae8f82aa
John T <reflectronic@outlook.com>
2018-06-21 14:09:29 -0400

    Removed calculator and added Start9

---
+++ Start9.Api.Contracts/Contracts.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.AddIn.Contract;
+using System.AddIn.Pipeline;
+using System.Collections;
+
+namespace Start9.Api.Contracts
+{
+    [AddInContract]
+    public interface IModuleContract : IContract
+    {
+        Message SendMessage<T>(Message message);
+        IConfiguration Configuration { get; }
+    }
+
+    public class Message
+    {
+        public String Text;
+    }
+
+    public class Message<T>
+    {
+        public T Object { get; }
+    }
+
+    public interface IConfiguration
+    {
+        Dictionary<String, Object> GetConfigurationStrings();
+    }
+
+    public interface ISkin : IConfiguration
+    {
+        IDictionary Resources { get; }
+    }
+}
